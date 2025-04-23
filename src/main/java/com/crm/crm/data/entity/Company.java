021a993eb15aa94569454b6375ca7a11e93a6947
mwijuka <mwijukaharoid@gmail.com>
2025-04-23 12:20:59 +0300

    added login view

--- src/main/java/com/crm/crm/data/entity/Company.java
+++ src/main/java/com/crm/crm/data/entity/Company.java
@@ -3,6 +3,8 @@ package com.crm.crm.data.entity;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.hibernate.annotations.Formula;
+
 import com.vaadin.flow.component.template.Id;
 
 import jakarta.annotation.Nullable;
@@ -12,7 +14,7 @@ import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Company  {
-    @Id
+
     @NotBlank
     private String name;
 
@@ -20,7 +22,13 @@ public class Company  {
     @Nullable
     private List<Contact> employees = new LinkedList<>();
 
-    
+    @Formula("(select count(c.id) from Contact c where c.company_id = id)")
+    private int employeeCount;
+
+    public int getEmployeeCount(){
+        return employeeCount;
+    }
+
     public String getName(){ return name;};
 
     public void setName(String name){ this.name  = name;}
